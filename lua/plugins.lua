1d5c019ef0a95e33d8fe7021e0bb69d2c94c970e
wuqichuang <wuqichuang@gmail.com>
2022-09-04 00:39:29 +0800

    Update plugins.lua
    "williamboman/nvim-lsp-installer" =>"williamboman/mason.nvim"

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -74,7 +74,11 @@ packer.startup({
     use("lukas-reineke/indent-blankline.nvim")
     --------------------- LSP --------------------
     -- use({ "williamboman/nvim-lsp-installer", commit = "36b44679f7cc73968dbb3b09246798a19f7c14e0" })
-    use({ "williamboman/nvim-lsp-installer" })
+    use({
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    })
+
     -- Lspconfig
     use({ "neovim/nvim-lspconfig" })
     -- 补全引擎
