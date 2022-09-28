ca10ef876ff792f8f1f45ae76861d73c92ca1347
wuqichuang <wuqichuang@gmail.com>
2022-09-28 11:31:13 +0800

    Update rust.lua
    use codelldb download by mason.nvim

--- lua/dap/nvim-dap/rust.lua
+++ lua/dap/nvim-dap/rust.lua
@@ -1,6 +1,5 @@
 -- 这里要修改为你的绝对路径
-local extension_path =
-  "/Users/nn/.local/share/nvim/site/pack/packer/start/vimspector/gadgets/macos/download/CodeLLDB/v1.6.10/root/extension/"
+local extension_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/"
 local codelldb_path = extension_path .. "adapter/codelldb"
 local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
