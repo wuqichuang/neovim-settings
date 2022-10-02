0b4b3279f30240b732a3e63b832d1463776fffbb
chuang <wuqichuang@gmail.com>
2022-10-02 22:31:51 +0800

    resolved_capabilities -> server_capabilities

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -71,7 +71,7 @@ null_ls.setup({
   diagnostics_format = "[#{s}] #{m}",
   on_attach = function(_)
     vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
-    -- if client.resolved_capabilities.document_formatting then
+    -- if client.server_capabilities.document_formatting then
     --   vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
     -- end
   end,
